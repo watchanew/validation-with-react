fd07b8371654c41a7b35f658033f21a0b1ec356a
Watcharasak Prommanee <73520880+watchanew@users.noreply.github.com>
2024-02-28 01:50:04 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Registration from "./Registration/Registration";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Registration />
+    </div>
+  );
+}
+export default App;
